8290fa730407bd824db3eb8bb1cdcdc1340cb2b2
Mike Kondrashov <kondrashovmischa@yandex.ru>
2024-01-31 14:31:18 +0300

    a

--- main.py
+++ main.py
@@ -6,27 +6,49 @@ app = Flask(__name__)
 
 # Функция для записи числа в файл
 def write_to_file(number, idUser):
-    with open('numbers.txt', 'a') as file:
+    with open('numbers.txt', 'a+', encoding="utf-8") as file:
         file.write(str(number) + " " + str(idUser) + '\n')
 
 
 # API endpoint для записи числа в файл
+
 @app.route('/write_number', methods=['POST'])
 def write_number():
     content = request.json
     number = content['number']
     idUser = content['iduser']
-    with open('numbers.txt', 'r') as file:
+    with open('numbers.txt', 'r', encoding="utf-8") as file:
         base = {int(line.strip().split()[0]): line.strip().split()[1] for line in file}
-    for key, value in base.items():
-        if value == idUser:
-            base[number] = base.pop(key)
-            break
-    with open('numbers.txt', 'w', encoding="windows-1251") as file:  # Открываем файл на перезапись
+    key = next(key for key, value in base.items() if value == idUser)
+    base[number] = base.pop(key)
+    base = dict(sorted(base.items(), reverse=True))
+    with open("numbers.txt", 'w', encoding="utf-8") as file:
         for key, item in base.items():
             file.write(f"{key} {item}\n")
-    sort_numbers_in_file()
+
     return jsonify({'message': 'Number has been written to the file'})
+
+
+@app.route('/mesto', methods=['POST'])
+def mesto():
+    content = request.json
+    idUser = content['iduser']
+    with open('numbers.txt', 'r', encoding="utf-8") as file:
+        base = {int(line.strip().split()[0]): line.strip().split()[1] for line in file}
+    for index, (key, value) in enumerate(base.items()):
+        if value == idUser:
+            return jsonify({"message": "ок", "index": index+1})
+
+
+@app.route('/top', methods=['POST'])
+def top():
+    # with open('numbers.txt', 'r') as file:
+    #     base = {int(line.strip().split()[0]): line.strip().split()[1] for line in file}
+    base = ""
+    with open('numbers.txt', 'r', encoding="utf-8") as file:
+        for line in file:
+            base += line.strip() + "lol"
+    return jsonify({'message': 'you are in base', 'top': base})
 
 
 @app.route('/can', methods=['POST'])
@@ -38,22 +60,34 @@ def can():
     if nick not in base:
         with open("users.txt", "a+", encoding="utf-8") as file:
             file.write(f"{nick}\n")
-        write_to_file(0,nick)
-        sort_numbers_in_file()
+        with open("numbers.txt", "a+", encoding="utf-8") as file:
+            file.write(f"0 {nick}\n")
         return jsonify({'message': 'nice', 'can': True})
     else:
         return jsonify({'message': 'you are in base', 'can': False})
 
 
+@app.route('/myresult', methods=['POST'])
+def my_result():
+    content = request.json
+    nick = content['nick']
+    with open('numbers.txt', 'r', encoding="utf-8") as file:
+        base = {int(line.strip().split()[0]): line.strip().split()[1] for line in file}
+    print(base)
+    keys = list(base.keys())
+    index = list(base.values()).index(nick)
+    key = keys[index]
+    return jsonify({'message': 'good', 'result': key})
+
+
 # Функция для сортировки чисел в файле
 def sort_numbers_in_file():
-    with open('numbers.txt', 'r') as file:
+    with open('numbers.txt', 'r', encoding="utf-8") as file:
         base = {int(line.strip().split()[0]): line.strip().split()[1] for line in file}
     base = dict(sorted(base.items(), reverse=True))
-    with open('numbers.txt', 'w', encoding="windows-1251") as file:  # Открываем файл на перезапись
+    with open('numbers.txt', 'w', encoding="utf-8") as file:  # Открываем файл на перезапись
         for key, item in base.items():
             file.write(f"{key} {item}\n")
-
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,12 +1,48 @@
-import random
-import time
-
 import requests
+import asyncio
+import asyncio
+import aiohttp
+
+urlServer = "https://591e-79-165-25-253.ngrok-free.app"
+
+
+async def mest(nick):
+    data = {"iduser": nick}
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{urlServer}/mesto", json=data) as response:
+            if response.status == 200:
+                response_json = await response.json()
+                return await wksajdbksaj(response_json['index'])
+            else:
+                return "-1"
+
+
+async def wksajdbksaj(number):
+    if number == 1:
+        return f"{number}st"
+    elif number == 2:
+        return f"{number}nd"
+    elif number == 3:
+        return f"{number}rd"
+    else:
+        return f"{number}th"
+
+
+def on_zapuck():
+    result = asyncio.run(mest("пингвин42"))
+    print(result)
+
+
+# Вызовите не-асинхронную функцию on_zapuck()
+on_zapuck()
+
+# Запустить обычную функцию on_zapuck()
+on_zapuck()
 
 
 # Отправка числа в API для записи в файл
-def send_number_to_api(number, iduser):
-    url = 'http://tinyurl.com/yc8mm6nn/write_number'
+async def send_number_to_api(number, iduser):
+    url = f'{urlServer}/write_number'
     data = {'number': number, 'iduser': iduser}
     response = requests.post(url, json=data)
     print(response.status_code)
@@ -15,20 +51,43 @@ def send_number_to_api(number, iduser):
         print('Number has been sent to the API and written to the file')
     else:
         print('Failed to send the number to the API')
+    await asyncio.sleep(1)
 
 
-def can(iduser):
-    url = 'http://tinyurl.com/yc8mm6nn/can'
+async def can(iduser):
+    url = f'{urlServer}/can'
     data = {'nick': iduser}
     response = requests.post(url, json=data)
     print(response.status_code)
     print(response.json())
+    await asyncio.sleep(1)
     if response.status_code == 200 and response.json()["can"]:
-        print('cool')
+        return True
     elif response.status_code == 200:
-        print(response.json()["message"])
+        return False
     else:
-        print('Failed to send the number to the API')
+        return False
 
 
-send_number_to_api(12323, "as")
+async def top():
+    url = f'{urlServer}/top'
+    response = requests.post(url)
+    await asyncio.sleep(1)
+    if response.status_code == 200:
+        return response.json()["top"].split("lol")
+    else:
+        return "иди лесом"
+
+
+async def result(nick):
+    data = {"nick": nick}
+    response = requests.post(f"{urlServer}/myresult", json=data)
+    await asyncio.sleep(1)
+    if response.status_code == 200:
+        return response.json()["result"]
+
+# def mesto(nick):
+#     data = {"iduser": nick}
+#     response = requests.post(f"{urlServer}/mesto", json=data)
+#     if response.status_code == 200:
+#         print(response.json()["index"])
